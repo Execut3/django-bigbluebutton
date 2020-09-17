3e0473e86ec7b3ea8efaf0df56b69042c112de0a
Execut3 <execut3.binarycodes@gmail.com>
2020-09-17 07:57:03 +0430

    localization added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='django-bigbluebutton',
-    version='0.1.0',
+    version='0.2.0',
     packages=['django_bigbluebutton'],
     description='A Django integration APP to connect django projects to Big Blue Button ;)',
     long_description=README,
@@ -19,7 +19,5 @@ setup(
         'Django>=2.0',
         'requests>=2.0'
     ],
-    package_data={
-        'django_bigbluebutton': ['migrations/*', 'templates/admin/*'],
-    },
+    include_package_data=True,
 )
